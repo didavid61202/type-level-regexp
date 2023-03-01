6f6925fe0d61a92ca8fac104cc42e831ed94ce50
David Tai <didavid61202@hotmail.com>
2023-03-01 17:50:33 +0800

    feat: implement 'g' global match

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ExhaustiveMatch } from './match'
+import { ExhaustiveMatch, GlobalMatch } from './match'
 import { ParseRegexp } from './parse'
 import { PermutationResult, ResolvePermutation } from './permutation'
 import {
@@ -13,20 +13,25 @@ export type FlagUnion = 'd' | 'g' | 'i' | 'm' | 's' | 'u' | 'y'
 
 export type MatchRegexp<
   InputString extends string,
-  Regexp extends string
+  Regexp extends string,
+  Flag extends FlagUnion | undefined = undefined
 > = ParseRegexp<Regexp> extends infer ParsedRegexpAST extends Matcher[]
   ? string extends InputString
     ? ResolvePermutation<ParsedRegexpAST> extends PermutationResult<
         infer MatchArray,
         infer NamedCaptures
       >
-      ? RegexpMatchResult<{
-          matched: MatchArray //TODO: string collape issue after TS 4.8, have to check if matchers include 'notCharSet' (check `[^` is in pattern?), 'notChar'... then union all array item with (string&{})
-          namedCaptures: NamedCaptures
-          input: InputString
-          restInput: undefined
-        }> | null
+      ? Flag extends 'g'
+        ? MatchArray[0][]
+        : RegexpMatchResult<{
+            matched: MatchArray //TODO: string collape issue after TS 4.8, have to check if matchers include 'notCharSet' (check `[^` is in pattern?), 'notChar'... then union all array item with (string&{})
+            namedCaptures: NamedCaptures
+            input: InputString
+            restInput: undefined
+          }> | null
       : never
+    : Flag extends 'g'
+    ? GlobalMatch<InputString, ParsedRegexpAST>
     : ExhaustiveMatch<InputString, ParsedRegexpAST> extends infer Result
     ? Result extends MatchedResult<
         infer MatchArray extends any[],
@@ -72,12 +77,12 @@ declare global {
       this: InputString,
       regexp: RE,
       flag?: Flag
-    ): MatchRegexp<InputString, RE>
+    ): MatchRegexp<InputString, RE, Flag>
 
     match<InputString extends string, RE extends string, Flag extends 'g'>(
       this: InputString,
       regexp: RE,
       flag: Flag
-    ): MatchRegexp<InputString, RE> //TODO: implement global match
+    ): MatchRegexp<InputString, RE, Flag>
   }
 }
