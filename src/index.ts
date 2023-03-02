5f0bf356b6682bb9b52a41091760b3f079e85467
David Tai <didavid61202@hotmail.com>
2023-03-02 23:04:12 +0800

    refactor `MatchRegexp` and `ReplaceWithRegexp`

--- src/index.ts
+++ src/index.ts
@@ -16,41 +16,38 @@ export type FlagUnion = 'd' | 'g' | 'i' | 'm' | 's' | 'u' | 'y'
 export type MatchRegexp<
   InputString extends string,
   Regexp extends string,
-  Flag extends FlagUnion | undefined = undefined
-> = ParseRegexp<Regexp> extends infer ParsedRegexpAST extends Matcher[]
-  ? string extends InputString
-    ? ResolvePermutation<ParsedRegexpAST> extends PermutationResult<
-        infer MatchArray,
-        infer NamedCaptures extends NamedCapturesTuple
-      >
-      ? Flag extends 'g'
-        ? MatchArray[0][]
-        : RegexpMatchResult<{
-            matched: MatchArray //TODO: string collape issue after TS 4.8, have to check if matchers include 'notCharSet' (check `[^` is in pattern?), 'notChar'... then union all array item with (string&{})
-            namedCaptures: NamedCaptures
-            input: InputString
-            restInput: undefined
-          }> | null
-      : never
-    : Flag extends 'g'
-    ? GlobalMatch<InputString, ParsedRegexpAST>
-    : Flag extends 'g'
-    ? GlobalMatch<InputString, ParsedRegexpAST>
-    : ExhaustiveMatch<InputString, ParsedRegexpAST> extends infer Result
-    ? Result extends MatchedResult<
-        infer MatchArray extends any[],
-        infer RestInputString extends string,
-        infer NamedCaptures extends NamedCapturesTuple
-      >
-      ? RegexpMatchResult<{
-          matched: MatchArray
+  Flag extends FlagUnion | undefined = undefined,
+  ParsedRegexpAST extends Matcher[] = ParseRegexp<Regexp>
+> = string extends InputString
+  ? ResolvePermutation<ParsedRegexpAST> extends PermutationResult<
+      infer MatchArray,
+      infer NamedCaptures extends NamedCapturesTuple
+    >
+    ? Flag extends 'g'
+      ? MatchArray[0][]
+      : RegexpMatchResult<{
+          matched: MatchArray //TODO: string collape issue after TS 4.8, have to check if matchers include 'notCharSet' (check `[^` is in pattern?), 'notChar'... then union all array item with (string&{})
           namedCaptures: NamedCaptures
           input: InputString
-          restInput: RestInputString
-        }>
-      : Result extends NullResult<any, any, any>
-      ? Result['results']
-      : never
+          restInput: undefined
+        }> | null
+    : never
+  : Flag extends 'g'
+  ? GlobalMatch<InputString, ParsedRegexpAST>
+  : ExhaustiveMatch<InputString, ParsedRegexpAST> extends infer Result
+  ? Result extends MatchedResult<
+      infer MatchArray extends any[],
+      infer RestInputString extends string,
+      infer NamedCaptures extends NamedCapturesTuple
+    >
+    ? RegexpMatchResult<{
+        matched: MatchArray
+        namedCaptures: NamedCaptures
+        input: InputString
+        restInput: RestInputString
+      }>
+    : Result extends NullResult<any, any, any>
+    ? Result['results']
     : never
   : never
 
@@ -79,29 +76,26 @@ export type ReplaceWithRegexp<
   InputString extends string,
   Regexp extends string,
   ReplaceValue extends string,
-  Flag extends 'g' | '' = ''
-> = ParseRegexp<Regexp> extends infer ParsedRegexpAST extends Matcher[]
-  ? Flag extends 'g'
-    ? GlobalReplace<InputString, ParsedRegexpAST, ReplaceValue>
-    : ExhaustiveMatch<InputString, ParsedRegexpAST> extends infer Result
-    ? Result extends MatchedResult<
-        infer MatchArray extends any[],
-        infer RestInputString extends string,
-        infer NamedCaptures extends NamedCapturesTuple
-      >
-      ? InputString extends `${infer Precedes}${MatchArray[0]}${RestInputString}`
-        ? `${Precedes}${ResolveRepalceValue<
-            ReplaceValue,
-            Precedes,
-            MatchArray,
-            RestInputString,
-            NamedCaptures
-          >}${RestInputString}`
-        : never
-      : Result extends NullResult<any, any, any>
-      ? Result['results']
+  Flag extends 'g' | '' = '',
+  ParsedRegexpAST extends Matcher[] = ParseRegexp<Regexp>
+> = Flag extends 'g'
+  ? GlobalReplace<InputString, ParsedRegexpAST, ReplaceValue>
+  : ExhaustiveMatch<InputString, ParsedRegexpAST> extends infer Result
+  ? Result extends MatchedResult<
+      infer MatchArray extends any[],
+      infer RestInputString extends string,
+      infer NamedCaptures extends NamedCapturesTuple
+    >
+    ? InputString extends `${infer Precedes}${MatchArray[0]}${RestInputString}`
+      ? `${Precedes}${ResolveRepalceValue<
+          ReplaceValue,
+          Precedes,
+          MatchArray,
+          RestInputString,
+          NamedCaptures
+        >}${RestInputString}`
       : never
-    : never
+    : InputString
   : never
 
 declare global {
