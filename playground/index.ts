fa9bead344d42837ea0ac3e24e011cfc793613a5
David Tai <didavid61202@hotmail.com>
2023-03-10 03:31:41 +0800

    patch: add playground examples

---
+++ playground/index.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createRegExp, MatchRegExp, ReplaceWithRegExp, ParseRegExp } from 'type-level-regexp'
+
+/**
+ *
+ * Note:
+ * Recommend to install vscode extension `vscode-twoslash-queries` for better DX,
+ * the extenstion will show inline type hints right after a special comment `// ^?`
+ *
+ */
+
+type MatchResult = MatchRegExp<
+  'MypA$3W0rd',
+  '^(?=.*(?<lower>[a-z]))(?=.*(?<upper>[A-Z]))(?=.*(?<digit>\\d))(?=.*(?<special>[!@#$%^&*])).{8,}$',
+  never
+>
+
+type Match = [
+  MatchResult['2'],
+  //           ^?
+  MatchResult['index'],
+  //             ^?
+  MatchResult['length'],
+  //             ^?
+  MatchResult['groups']
+  //             ^?
+]
+
+type ReplaceReulst = ReplaceWithRegExp<
+  //    ^?
+  'foo-bar-baz',
+  '(?<=f[a-z]O\\W)b(?<g1>[A|B])r',
+  'qux-$&-$`d$<g1>o',
+  'i'
+>
+
+const RE = createRegExp(
+  '(?<=Nuxt )(?<type>.{4,}?) site at (?<protocal>https?)(:\\/\\/)(?:www.)?(?<secondDomain>[a-zA-Z0-9@:%._+~#=]{2,40})\\.(?<topDomain>[a-z]{2,6})(?<path>\\/[a-zA-Z0-9@:%._+~#=]{2,20})*'
+)
+
+const chainedResult =
+  //     ^?
+  `Check out the Nuxt documentation 📖 site at https://nuxt.com/docs 👉 it's the best resource for clear and concise explanations, with excellent examples that make web development a breeze! ❤️`
+    .replace(RE, 'starter templates site at $<protocal>$3$<secondDomain>.new')
+    .replace(
+      createRegExp("it's the BeSt resource f[A-Z]r (.{10,20})explanations", ['i']),
+      "it's the best place to start a new awesome website of any kind ❤️, and it has some $1starter templates 🚀"
+    )
+    .replace(createRegExp('❤️|👉', ['g']), '💚')
+    .match(RE)
+
+const result = [
+  chainedResult[4],
+  //            ^?
+  chainedResult.index,
+  //             ^?
+  chainedResult.length,
+  //             ^?
+  chainedResult.input,
+  //             ^?
+]
+
+type Parsed = ParseRegExp<'(?<g1>foo)_(?<g2>bar_(?<g3>baz))'>
+//     ^?
